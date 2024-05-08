8f92a088c57bf276745c26b4729402c64faed715
Tereza Beránková <xteryna@seznam.cz>
2024-05-08 22:43:29 +0200

    folder img copied to folder pages in order to test path to img files

---
+++ src/pages/index.jsx
@@ -0,0 +1,37 @@
+import { render } from '@czechitas/render';
+import '../global.css';
+import './index.css';
+
+
+const name1 = 'Silvestr';
+const image1 = "../img/silvestr.jpg";
+const text1 =
+  'Silvestr rád pozoruje dění za oknem a upřímně se usmívá na všechno kolemjdoucí.';
+
+const name2 = 'Ctirad';
+const image2 = 'img/ctirad.jpg';
+const text2 =
+  'Ctirad tráví svůj čas v blízkosti lednice a s očekáváním pozoruje její bílé dveře.';
+
+
+const Plysak = (props) => {
+  return (
+    <div className="plushy">
+      <img className="plushy__image" src={props.obrazek}/>
+      <h2 className="plushy__name">{props.jmenoPlysaka}</h2>
+      <p className="plushy__text">{props.textOPlysakovi}</p>
+    </div>
+  )
+}
+
+
+document.querySelector('#root').innerHTML = render(
+  <>
+    <h1>Plyšáci</h1>
+    <div className="plushies">
+      <Plysak jmenoPlysaka = {name2} textOPlysakovi = {text2} obrazek = {image2}/>
+      <Plysak jmenoPlysaka = {name1} textOPlysakovi = {text1} obrazek = {image1}/>
+
+    </div>
+  </>
+);
